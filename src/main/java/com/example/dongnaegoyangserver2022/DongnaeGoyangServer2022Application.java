3086f6c72e82e6d7217fb5b3c8fcf98b1b85b61b
jeeheaG <jeehea99@naver.com>
2022-11-21 21:18:12 +0900

    Fix : Try to fix JPA Auditing timezone but failed

--- src/main/java/com/example/dongnaegoyangserver2022/DongnaeGoyangServer2022Application.java
+++ src/main/java/com/example/dongnaegoyangserver2022/DongnaeGoyangServer2022Application.java
@@ -4,9 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class DongnaeGoyangServer2022Application {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+//        System.out.println("현재 시각 : "+ new Date());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DongnaeGoyangServer2022Application.class, args);
